1150bcc53c137f40839eb2bb740ca5a0cc75dfa0
Sasha Azad <sazad@ncsu.edu>
2024-02-13 16:06:42 -0500

    initializing Anthology files

--- Models/Motive.cs
+++ Models/Motive.cs
@@ -1,75 +1,43 @@
-﻿using Anthology.Controllers;
-
-namespace Anthology.Models
+﻿namespace Anthology.Models
 {
-    /** Motive class all motive types should inherit from */
-    public abstract class Motive
+    /// <summary>
+    /// Motive class all motive types should inherit from.
+    /// </summary>
+    public static class Motive
     {
-        /** all motives should have a type */
-        public abstract string Type { get; }
-
-        /** all motives should have a numeric amount */
-        public float Amount { get; set; }
-
-        /** the maximum value of a motive */
+        /// <summary>
+        /// The maximum value of a motive.
+        /// </summary>
         public const float MAX = 5f;
 
-        /** the minimum value of a motive */
+        /// <summary>
+        /// The minimum value of a motive.
+        /// </summary>
         public const float MIN = 1f;
 
-        /** the string name of physical type motives */
+        /// <summary>
+        /// The string name of physical type motives.
+        /// </summary>
         public const string PHYSICAL = "physical";
 
-        /** the string name of emotional type motives */
+        /// <summary>
+        /// The string name of emotional type motives.
+        /// </summary>
         public const string EMOTIONAL = "emotional";
 
-        /** the string name of social type motives */
+        /// <summary>
+        /// The string name of social type motives.
+        /// </summary>
         public const string SOCIAL = "social";
 
-        /** the string name of financial type motives */
+        /// <summary>
+        /// The string name of financial type motives.
+        /// </summary>
         public const string FINANCIAL = "financial";
 
-        /** the string name of accomplishment type motives */
+        /// <summary>
+        /// The string name of accomplishment type motives.
+        /// </summary>
         public const string ACCOMPLISHMENT = "accomplishment";
-    }
-
-    public class MPhysical : Motive
-    {
-        public override string Type
-        {
-            get { return PHYSICAL; }
-        }
-    }
-
-    public class MEmotional : Motive
-    {
-        public override string Type
-        {
-            get { return EMOTIONAL; }
-        }
-    }
-
-    public class MSocial : Motive
-    {
-        public override string Type
-        {
-            get { return SOCIAL; }
-        }
-    }
-
-    public class MFinancial : Motive
-    {
-        public override string Type
-        {
-            get { return FINANCIAL; }
-        }
-    }
-
-    public class MAccomplishment : Motive
-    {
-        public override string Type
-        {
-            get { return ACCOMPLISHMENT; }
-        }
     }
 }
--- Models/World.cs
+++ Models/World.cs
@@ -1,22 +1,37 @@
-﻿using Anthology.Controllers;
-
-namespace Anthology.Models
+﻿namespace Anthology.Models
 {
+    /// <summary>
+    /// Simulation environment that encompasses the real world.
+    /// </summary>
     public static class World
     {
-        /** World time, or ticks */
+        /// <summary>
+        /// World time, or ticks.
+        /// </summary>
         public static int Time { get; set; } = 0;
 
-        /** Initialize/reset all static world variables */
+        /// <summary>
+        /// Json parser to use for file I/O, can be swapped from compatibility.
+        /// </summary>
+        public static JsonRW ReadWrite { get; set; } = new NetJson();
+
+        /// <summary>
+        /// Initialize/reset all static world variables.
+        /// </summary>
+        /// <param name="actionPath">Path of JSON file to load actions from.</param>
+        /// <param name="agentPath">Path of JSON file to load agents from.</param>
+        /// <param name="locationPath">Path of JSON file to load locations from.</param>
         public static void Init(string actionPath, string agentPath, string locationPath)
         {
             Time = 0;
             ActionManager.Init(actionPath);
             AgentManager.Init(agentPath);
-            LocationManager.Init(UI.GridSize, locationPath);
+            LocationManager.Init(locationPath);
         }
 
-        /** Increment simulation time by one tick */
+        /// <summary>
+        /// Increment simulation time by one tick.
+        /// </summary>
         public static void IncrementTime()
         {
             Time += 1;
