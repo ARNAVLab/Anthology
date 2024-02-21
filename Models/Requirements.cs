1a7cd8eeb38eba421c3727b9a144c97763be29ed
Sasha Azad <sazad@ncsu.edu>
2024-02-21 14:44:50 -0500

    edited filter, fixed merge conflicts

--- Models/Requirements.cs
+++ Models/Requirements.cs
@@ -250,9 +250,9 @@ namespace Anthology.Models
 		public List<Requirement> GetRequirementsByType(string r_type)
         {
             List<Requirement> reqs = new();
-			if (r_type == Requirement.LOCATION and Locations != null) reqs.AddRange(Locations);
-            else if (r_type == Requirement.PEOPLE and People != null) reqs.AddRange(People);
-			if (r_type == Requirement.MOTIVE and Motives != null) reqs.AddRange(Motives);
+			if (r_type == Requirement.LOCATION && Locations != null) reqs.AddRange(Locations);
+            else if (r_type == Requirement.PEOPLE && People != null) reqs.AddRange(People);
+			if (r_type == Requirement.MOTIVE && Motives != null) reqs.AddRange(Motives);
             return reqs;
         }
     }
